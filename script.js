01a54930ad6139e2b029dbb967e2127be31360bb
Milana <milana.zaborovska@gmail.com>
2023-07-01 23:19:54 +0100

    add create divs function to js

--- script.js
+++ script.js
@@ -3,9 +3,31 @@ const slider = document.getElementById("myRange");
 const output = document.getElementById("sliderValue");
 output.innerHTML = (`${slider.value} x ${slider.value}`);
 
-slider.oninput = function() {
-  output.innerHTML = (`${this.value} x ${this.value}`);
+
+// Add divs and position divs 
+
+let canvasSize = slider.value;
+
+
+//let canvasSize = slider.value;
+
+const createDivs = (numOfDivs) => {
+    for(let i = 0; i < numOfDivs; i++){
+        let element = document.createElement("div");
+        element.classList.add("canvasItem");
+        document.getElementById('canvas').appendChild(element);
+    }
+};
+
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
 }
 
-// Change canvas size
+// changes text when slider used and creates divs
+
+slider.oninput = function() {
+    output.innerHTML = (`${this.value} x ${this.value}`);
+  }
 
