d17b1421ed8e3975cce26de18d29aaf23f3d8413
Milana <milana.zaborovska@gmail.com>
2023-07-02 22:02:19 +0100

    comment script

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
-// Slider number display
+// Slider value 
 const slider = document.getElementById("myRange");
+// Text on top of slider value
 const output = document.getElementById("sliderValue");
+// Sets text at the start of page loading to the default slider value
 output.innerHTML = `${slider.value} x ${slider.value}`;
 
-// Add divs and position divs
-let canvasSize = slider.value;
+// Canvas in variable
 const canvas = document.getElementById('canvas');
 
+// Creates divs by looping up until the slider value, creating divs and making them a child of canvas
 const createDivs = (numOfDivs) => {
   for (let i = 0; i < numOfDivs; i++) {
     let element = document.createElement("div");
     element.classList.add("canvasItem");
     canvas.appendChild(element);
   }
-
+  // Setting grid columns and rows based on slider value
   canvas.style.gridTemplateColumns = `repeat(${Math.sqrt(numOfDivs)}, 1fr)`;
   canvas.style.gridTemplateRows = `repeat(${Math.sqrt(numOfDivs)}, 1fr)`;
 };
@@ -27,7 +29,7 @@ const removeAllChildNodes = (parent) => {
 // Global variable to track the state of the left mouse button
 let isDrawing = false;
 
-// Changes text when slider used and creates divs
+// Changes text when slider used and creates divs and assigns event listeners
 slider.oninput = function() {
   output.innerHTML = `${this.value} x ${this.value}`;
   removeAllChildNodes(canvas);
@@ -35,8 +37,10 @@ slider.oninput = function() {
   addCanvasElementEvent();
 }
 
+// The div in variable
 const canvasElements = document.getElementsByClassName("canvasItem");
 
+// if mouse is left clicked and held and hovered over div then colour is changed
 const addCanvasElementEvent = () => {
   for (let i = 0; i < canvasElements.length; i++) {
     canvasElements[i].addEventListener('mousedown', (e) => {
@@ -57,6 +61,7 @@ const addCanvasElementEvent = () => {
 };
 
 // Create divs with initial size based on slider value
+let canvasSize = slider.value;
 createDivs(canvasSize * canvasSize);
 
 // Add event listeners to canvas elements
@@ -64,7 +69,6 @@ addCanvasElementEvent();
 
 
 // For clear button:
-
 const clearButton = (e) => {
     for (let i = 0; i < canvasElements.length; i++){
         canvasElements[i].style.backgroundColor = 'white';
